fa776b849ebda356a6b87d09d9e9fac51b59bec4
raBeta <raul.bethencourt.pro@gmail.com>
2022-09-13 22:02:10 +0200

    :construction: get logs from request

--- myApp.js
+++ myApp.js
@@ -2,22 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const root = __dirname + '/views/index.html';
-const public = __dirname + '/public';
+const assets = __dirname + '/public';
 
-app.use('/public', express.static(public));
+app.use('/public', express.static(assets));
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(root);
+	res.sendFile(root);
 });
 
 app.get('/json', (req, res) => {
-    let txt = 'Hello json';
+	let txt = 'Hello json';
 
-    if (process.env.MESSAGE_STYLE == 'uppercase') {
-        var json_txt = txt.toUpperCase();
-    }
+	if (process.env.MESSAGE_STYLE == 'uppercase') {
+		json_txt = txt.toUpperCase();
+	} else {
+		json_txt = txt;
+	}
 
-    res.json({ message: json_txt });
+	res.json({ message: json_txt });
 });
 
 module.exports = app;
