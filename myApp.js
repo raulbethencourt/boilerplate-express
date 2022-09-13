dafa3a7b8cb54c11a77be7dfb393a973085f2dcc
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2022-09-13 12:00:47 +0200

    :construction: start with paths

--- myApp.js
+++ myApp.js
@@ -3,38 +3,8 @@ let app = express();
 
 console.log('Hello World');
 
+app.get('/', function (req, res) {
+    res.send('Hello Express');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
