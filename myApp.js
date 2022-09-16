b443d367d7cae586c25cce3fc4e5db0e0a40425c
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2022-09-16 16:40:13 +0200

    :sparkles: finish

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const root = __dirname + '/views/index.html';
 const assets = __dirname + '/public';
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(assets));
 
 app.use((req, res, next) => {
@@ -35,5 +37,18 @@ app.get(
         res.json({ time: req.time });
     }
 );
+
+app.get('/:word/echo', (req, res, next) => {
+    let word = req.params.word;
+    res.json({ echo: word });
+});
+
+app.route('/name')
+    .get((req, res, next) => {
+        res.json({ name: req.query.first + ' ' + req.query.last });
+    })
+    .post((req, res) => {
+        res.json({ name: req.body.first + ' ' + req.body.last });
+    });
 
 module.exports = app;
