b4e773647bd0582521564f834424674f11df34aa
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2022-09-16 14:32:46 +0200

    :construction: add middelware

--- myApp.js
+++ myApp.js
@@ -7,24 +7,33 @@ const assets = __dirname + '/public';
 app.use('/public', express.static(assets));
 
 app.use((req, res, next) => {
-	console.log(`${req.method} ${req.path} - ${req.ip}`);
-	next();
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
 });
 
 app.get('/', (req, res) => {
-	res.sendFile(root);
+    res.sendFile(root);
 });
 
 app.get('/json', (req, res) => {
-	let txt = 'Hello json';
+    let json_txt = 'Hello json';
 
-	if (process.env.MESSAGE_STYLE == 'uppercase') {
-		json_txt = txt.toUpperCase();
-	} else {
-		json_txt = txt;
-	}
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        json_txt = json_txt.toUpperCase();
+    }
 
-	res.json({ message: json_txt });
+    res.json({ message: json_txt });
 });
+
+app.get(
+    '/now',
+    (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
+    (req, res) => {
+        res.json({ time: req.time });
+    }
+);
 
 module.exports = app;
